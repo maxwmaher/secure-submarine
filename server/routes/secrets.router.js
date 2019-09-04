9ec71ea9cae65a4a6300d90350518e6eaa6ef4c4
Max Maher <maxwmaher@gmail.com>
2019-09-04 14:59:36 -0500

    finished base mode git ad d.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    let clearance = req.user.clearance_level;
+    let queryText = `SELECT * FROM "secret" WHERE secrecy_level <= $1;`;
+    pool.query(queryText, [clearance])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
 });
+
 
 module.exports = router;
